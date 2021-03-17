e1bc0f7b3faad48ae027380fd053856ef02bb4a7
Jose Pizarro <josmapizarro.99@gmail.com>
2021-03-16 21:03:52 -0400

    Creacion de modelo,controlador y publicacion de apiDoble

---
+++ apiDoble/Controllers/DataController.cs
@@ -0,0 +1,39 @@
+﻿
+namespace apiDoble.Controllers
+{
+    using System;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+    using Azure.Messaging.ServiceBus;
+    using Newtonsoft.Json;
+    using apiDoble.Models;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+        [HttpPost]
+
+        public async Task<bool> EnviarAsync([FromBody] Data data)
+        {
+            string connectionString = "Endpoint=sb://josepizarro.servicebus.windows.net/;SharedAccessKeyName=Enviar;SharedAccessKey=3cdUpVCNX3wYroXXMZJlna2IQxq0wy7G2XUvzf6MwEU=;EntityPath=qpar";
+            string queueName = "qpar";
+            string mensaje = JsonConvert.SerializeObject(data);
+        
+            
+            await using (ServiceBusClient client = new ServiceBusClient(connectionString))
+            {
+                // create a sender for the queue 
+                ServiceBusSender sender = client.CreateSender(queueName);
+
+                // create a message that we can send
+                ServiceBusMessage message = new ServiceBusMessage(mensaje);
+
+                // send the message
+                await sender.SendMessageAsync(message);
+                Console.WriteLine($"Sent a single message to the queue: {queueName}");
+            }
+            return true;
+        }
+    }
+}
---
+++ apiDoble/Models/Data.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apiDoble.Models
+{
+    public class Data
+    {
+        [Key]
+        [DataType(DataType.DateTime)]
+        public DateTime FechaHora { get; set; }
+        [Required]
+        public int Random { get; set; }
+    }
+}
